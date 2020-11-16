af49c8cdaf48a7e408347624596b956099288557
unknown <c00224660@itcarlow.ie>
2020-11-16 03:09:44 +0000

    Feature 1 complete

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray; // [,] = how to define an array with 2 dimensions
+    private TextMesh[,] textArray;
+    private Vector3 originPosition;
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+        textArray = new TextMesh[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                textArray[x,y] = CreateWorldText(null, gridArray[x, y].ToString(), GetWorldPosition(x, y) + new Vector3(cellSize,cellSize) * .5f, 7, Color.white, TextAnchor.MiddleCenter, TextAlignment.Center);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+    // Create Text in World
+    private static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment)//, int sortingOrder)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color;
+        //textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+    }
+
+    // Get world pos of tile
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    private Vector2Int GetXY(Vector3 worldPosition)
+    {
+        return new Vector2Int(Mathf.FloorToInt((worldPosition.x - originPosition.x) / cellSize), Mathf.FloorToInt((worldPosition.y - originPosition.y) / cellSize));
+    }
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height) // check for valid tile
+        {
+            gridArray[x, y] = value;
+            textArray[x, y].text = gridArray[x, y].ToString();
+        }  
+    }
+    public void SetValue(Vector3 worldPosition, int value)  // overloaded to above function, for purpose of ease of calling
+    {
+        SetValue(GetXY(worldPosition).x, GetXY(worldPosition).y, value);
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+    private int tileWidth;
+    private int tileHeight;
+    private float cellSize;
+    private Vector3 mapOriginPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tileWidth = 16;
+        tileHeight = 9;
+        cellSize = 1f;
+        mapOriginPosition = new Vector3(0, 0, 0);  // set origin
+        grid = new Grid(tileWidth, tileHeight, cellSize, mapOriginPosition);
+        Camera.main.transform.position = mapOriginPosition + new Vector3((tileWidth * cellSize) / 2, (tileHeight * cellSize) / 2, -10);
+        //Camera.main.transform.localScale.Set(2, 2,2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), 2);
+        }
+    }
+}
